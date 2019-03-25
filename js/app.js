191e36fb4e23fc1b8341d65a92d1bb0bdf663304
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-03-25 15:48:06 -0600

    Take it Easy completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 console.log('loaded'); 
+
+// PART 1 DATA TYPES
+
+	// 1. Booleans
+	// 2. String
+	// 3. Array
+	// 4. Array
+	// 5. OBjects
+	// 6. Objects qith arrays within them 
+
+// PART 2 TAKE IT EASY
+
+// 1
+var rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+
+console.log(rainbow[4]); 
+
+// 2
+
+const Veda = {
+	Age: 22,
+	Hobby: 'Sketching',
+	Town: "Arvada",
+	Datatype: "Objects"
+}
+
+console.log(Veda.Hobby);
+
+
